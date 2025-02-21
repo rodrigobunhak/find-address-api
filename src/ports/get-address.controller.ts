713a336e94df8a3de4c4ed362259b1fdb9f02e23
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2025-02-21 09:45:29 -0300

    Add api documentation

--- src/ports/get-address.controller.ts
+++ src/ports/get-address.controller.ts
@@ -3,11 +3,28 @@ import { GetAddressUseCase } from 'src/application/usecase/get-address.usecase';
 import { InvalidCepError } from 'src/domain/errors/domain.error';
 import { CepNotFoundError } from 'src/infra/errors/infra.error';
 import { GetAddressInputDto, GetAddressOutputDto } from './get-address.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('cep')
 @Controller('cep')
 export class GetAddressController {
   constructor(private readonly getAddressUseCase: GetAddressUseCase) {}
 
+  @ApiOperation({
+    summary: 'Get Address by cep',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: GetAddressOutputDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: CepNotFoundError.message,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: InvalidCepError.message,
+  })
   @Get(':cep')
   async getAddress(@Param() input: GetAddressInputDto): Promise<GetAddressOutputDto> {
     const result = await this.getAddressUseCase.execute({ cep: input.cep });
--- src/ports/get-address.dto.ts
+++ src/ports/get-address.dto.ts
@@ -1,15 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { GetAddressInput, GetAddressOutput } from 'src/application/dto/get-address.dto';
 
 export class GetAddressInputDto implements GetAddressInput {
+  @ApiProperty({ example: '01001000' })
   cep: string;
 }
 
 export class GetAddressOutputDto implements GetAddressOutput {
+  @ApiProperty()
   cep: string;
+
+  @ApiProperty()
   uf: string;
+
+  @ApiProperty()
   state: string;
+
+  @ApiProperty()
   city: string;
+
+  @ApiProperty()
   neighborhood: string;
+
+  @ApiProperty()
   street: string;
+
+  @ApiProperty()
   fullAddress: string;
 }
